7f275564cf84236997bafe3674c28019c325d730
Nabeel Ahmad <nabeelbna@gmail.com>
2019-06-21 11:56:44 -0400

    fixed api views and hello view added

--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from profiles_api import views
 
 
-urlpatters = [
-path('hello-view/', views.HelloApiView.as_view()),
+urlpatterns = [
+    path('hello-view/', views.HelloApiView.as_view()),
 ]
--- profiles_api/views.py
+++ profiles_api/views.py
@@ -7,11 +7,12 @@ class HelloApiView(APIView):
 
     def get(self, request, format=None):
         """Returns a list of APIView features"""
+
         an_apiview = [
-        'Uses HTTP methods as function (get, post, patch, put delete)',
-        'Is similar to a traditional Django View',
-        'Gives you the most control over your application logic',
-        'Is mapped manually to URLs',
+            'Uses HTTP methods as functions (get, post, patch, put, delete)',
+            'Is similar to a traditional Django View',
+            'Gives you the most control over your logic',
+            'Is mapped manually to URLs',
         ]
 
-        return Response({'message': 'Hello!', 'an apiview': an_apiview})
+        return Response({'message': 'Hello!', 'an_apiview': an_apiview})
